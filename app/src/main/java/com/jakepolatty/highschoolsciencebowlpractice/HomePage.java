b12f4a4dc5821675807bc9820952113b3618f699
jakepolatty <jakepolatty@gmail.com>
2017-09-07 18:45:39 -0400

    Styled spinner dropdowns

--- app/src/main/java/com/jakepolatty/highschoolsciencebowlpractice/HomePage.java
+++ app/src/main/java/com/jakepolatty/highschoolsciencebowlpractice/HomePage.java
@@ -1,5 +1,6 @@
 package com.jakepolatty.highschoolsciencebowlpractice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -19,28 +20,33 @@ public class HomePage extends AppCompatActivity {
         readerModeOptionButton = (Button) findViewById(R.id.readerModeOptionButton);
         studyModeOptionButton = (Button) findViewById(R.id.studyModeOptionButton);
 
-        View.OnClickListener quizModeListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("Quiz Mode");
-            }
-        };
-        quizModeOptionButton.setOnClickListener(quizModeListener);
+//        View.OnClickListener quizModeListener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                System.out.println("Quiz Mode");
+//            }
+//        };
+//        quizModeOptionButton.setOnClickListener(quizModeListener);
+//
+//        View.OnClickListener readerModeListener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                System.out.println("Reader Mode");
+//            }
+//        };
+//        readerModeOptionButton.setOnClickListener(readerModeListener);
+//
+//        View.OnClickListener studyModeListener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                System.out.println("Study Mode");
+//            }
+//        };
+//        studyModeOptionButton.setOnClickListener(studyModeListener);
+    }
 
-        View.OnClickListener readerModeListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("Reader Mode");
-            }
-        };
-        readerModeOptionButton.setOnClickListener(readerModeListener);
-
-        View.OnClickListener studyModeListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("Study Mode");
-            }
-        };
-        studyModeOptionButton.setOnClickListener(studyModeListener);
+    public void startQuizMode(View view) {
+        Intent intent = new Intent(HomePage.this, QuizModeSettingsActivity.class);
+        startActivity(intent);
     }
 }
