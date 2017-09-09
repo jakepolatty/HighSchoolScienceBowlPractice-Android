9b3cbd7591c27cd1b65e8f8a0fc88e37d8c4dc0c
jakepolatty <jakepolatty@gmail.com>
2017-09-09 19:41:53 -0400

    Added home page logo

--- app/src/main/java/com/jakepolatty/highschoolsciencebowlpractice/ui/HomePage.java
+++ app/src/main/java/com/jakepolatty/highschoolsciencebowlpractice/ui/HomePage.java
@@ -10,9 +10,6 @@ import com.jakepolatty.highschoolsciencebowlpractice.R;
 import com.jakepolatty.highschoolsciencebowlpractice.model.QuestionJSONParser;
 
 public class HomePage extends AppCompatActivity {
-    private Button quizModeOptionButton;
-    private Button readerModeOptionButton;
-    private Button studyModeOptionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +17,6 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
         QuestionJSONParser.getInstance(getApplicationContext());
-
-        quizModeOptionButton = (Button) findViewById(R.id.quizModeOptionButton);
-        readerModeOptionButton = (Button) findViewById(R.id.readerModeOptionButton);
-        studyModeOptionButton = (Button) findViewById(R.id.studyModeOptionButton);
     }
 
     public void openQuizModeSettings(View view) {
