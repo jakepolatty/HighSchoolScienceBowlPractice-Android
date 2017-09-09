c6c0f7bcb0fd4272baf04c2ddcb1c7e1233a29dd
jakepolatty <jakepolatty@gmail.com>
2017-09-09 11:24:29 -0400

    Implemented set and round picker layout

---
+++ app/src/main/java/com/jakepolatty/highschoolsciencebowlpractice/ui/ReaderModePage.java
@@ -0,0 +1,15 @@
+package com.jakepolatty.highschoolsciencebowlpractice.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jakepolatty.highschoolsciencebowlpractice.R;
+
+public class ReaderModePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reader_mode_page);
+    }
+}
---
+++ app/src/main/java/com/jakepolatty/highschoolsciencebowlpractice/ui/ReaderModeSettingsPage.java
@@ -0,0 +1,15 @@
+package com.jakepolatty.highschoolsciencebowlpractice.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jakepolatty.highschoolsciencebowlpractice.R;
+
+public class ReaderModeSettingsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reader_mode_settings_page);
+    }
+}
