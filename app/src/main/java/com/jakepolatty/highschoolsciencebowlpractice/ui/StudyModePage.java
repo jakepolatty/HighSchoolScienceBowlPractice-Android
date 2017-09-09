61f345ec2ee891160cb01fb7be12fc359390f4a6
jakepolatty <jakepolatty@gmail.com>
2017-09-08 21:00:52 -0400

    Built out study mode page layout

--- app/src/main/java/com/jakepolatty/highschoolsciencebowlpractice/ui/StudyModePage.java
+++ app/src/main/java/com/jakepolatty/highschoolsciencebowlpractice/ui/StudyModePage.java
@@ -2,10 +2,30 @@ package com.jakepolatty.highschoolsciencebowlpractice.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.jakepolatty.highschoolsciencebowlpractice.R;
+import com.jakepolatty.highschoolsciencebowlpractice.model.Question;
 
 public class StudyModePage extends AppCompatActivity {
+    // Question text fields
+    private TextView roundSetNumLabel;
+    private TextView questionNumLabel;
+    private TextView categoryTypeLabel;
+    private TextView questionTextLabel;
+    private TextView answerOptionsLabel;
+    private TextView answerLabel;
+
+    // Toolbar Buttons
+    private Button menuButton;
+    private Button nextButton;
+
+    // Intent fields
+    private String category;
+    private int round;
+
+    private Question question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
