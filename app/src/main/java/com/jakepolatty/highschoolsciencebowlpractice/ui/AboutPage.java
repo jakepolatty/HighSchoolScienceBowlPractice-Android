60561d030aa83d4b08430292948fadaae0da17ed
Jake Polatty <jakepolatty@gmail.com>
2017-09-09 17:08:34 -0400

    Added home and about pages

---
+++ app/src/main/java/com/jakepolatty/highschoolsciencebowlpractice/ui/AboutPage.java
@@ -0,0 +1,15 @@
+package com.jakepolatty.highschoolsciencebowlpractice.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jakepolatty.highschoolsciencebowlpractice.R;
+
+public class AboutPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_page);
+    }
+}
---
+++ app/src/main/java/com/jakepolatty/highschoolsciencebowlpractice/ui/HelpPage.java
@@ -0,0 +1,15 @@
+package com.jakepolatty.highschoolsciencebowlpractice.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jakepolatty.highschoolsciencebowlpractice.R;
+
+public class HelpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_page);
+    }
+}
